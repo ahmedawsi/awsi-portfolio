8bfa94ff51fda7fb2dc5c5ec446319d1fa3321d4
Ahmed Awsi <ah9med0@gmail.com>
2023-11-25 00:18:56 +0300

    1. Optimized loading speed by compressing images 2. Added email functionality

--- app/components/work-post.js
+++ app/components/work-post.js
@@ -1,16 +1,30 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useRef, useLayoutEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function WorkPost({ title, imgSrc, figProtoLink }) {
+  const [isOpen, setIsOpen] = useState(true);
+
   return (
-    <div className="flex flex-col h-fit w-full aspect-[2/3] gap-4 p-4 bg-[#1a1a1a] rounded-xl">
+    <div
+      layout
+      className="flex flex-col w-full gap-4 p-4 bg-[#1e1e1e] rounded-xl overflow-y-hidden"
+      onClick={() => {
+        setIsOpen(!isOpen);
+      }}
+    >
       <img
+        layout="position"
         src={imgSrc}
         className="object-cover w-full h-full rounded-md drop-shadow-[0_5px_8px_rgba(0,0,0,0.5)]"
       />
-      <div className="flex flex-row items-center justify-between gap-5">
+
+      <div
+        layout="position"
+        className="flex flex-row items-center justify-between gap-5"
+      >
         <p className="py-[5px]">{title}</p>
         {figProtoLink?.length ? (
           <a
@@ -23,13 +37,37 @@ export default function WorkPost({ title, imgSrc, figProtoLink }) {
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 20 20"
               fill="#ffffff"
-              class="w-4 h-4"
+              className="w-4 h-4"
             >
               <path d="M6.3 2.841A1.5 1.5 0 004 4.11V15.89a1.5 1.5 0 002.3 1.269l9.344-5.89a1.5 1.5 0 000-2.538L6.3 2.84z" />
             </svg>
           </a>
         ) : null}
       </div>
+
+      {/* <AnimatePresence>
+        <motion.div
+          id="details_div"
+          layout="position"
+          transition={{
+            duration: 1,
+            ease: "easeInOut",
+            type: "spring",
+          }}
+          ref={detailsRef}
+          initial={{
+            opacity: 0,
+          }}
+          animate={{
+            opacity: 1,
+          }}
+          exit={{ opacity: 0 }}
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+          recusandae aliquam optio fugiat labore! Nobis quod quia accusamus
+          ipsum veniam.
+        </motion.div>
+      </AnimatePresence> */}
     </div>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,8 +1,37 @@
-import Image from "next/image";
+"use client";
+
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import WorkPost from "./components/work-post";
 import SocialCard from "./components/social-card";
+import {motion} from "framer-motion";
 
 export default function Home() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_fnjxw3b",
+        "template_21qx3nv",
+        form.current,
+        "oxXzwTIDXRSXDdIpY"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          e.target.reset();
+          alert("Sent successfully!");
+        },
+        (error) => {
+          console.log(error.text);
+          alert("Something went wrong while sending your message.");
+        }
+      );
+  };
+
   return (
     <div>
       <div className="h-screen min-w-screen">
@@ -17,7 +46,7 @@ export default function Home() {
 
           <div className="flex self-center -translate-x-[3.75px]">
             <div className="absolute w-[7px] h-[7px] rounded-full bg-gray-50" />
-            {/* <div className="absolute w-[7px] h-[7px] rounded-full bg-gray-50 animate-ping"/> */}
+            <div className="absolute w-[7px] h-[7px] rounded-full bg-gray-50 animate-ping " />
           </div>
           {/* <div className="self-center w-[7px] h-[7px] rounded-full bg-gray-50"/> */}
           <div className="w-[1.5px] h-[185px] bg-gray-400 self-center" />
@@ -26,42 +55,59 @@ export default function Home() {
         <img
           src="media/ahmedphoto.JPG"
           className="z-0 object-cover w-full h-full opacity-20"
+          alt="A photo of Ahmed"
         />
       </div>
       <div className="flex flex-col justify-center w-full h-fit">
         <div className="h-fit pt-[90px] w-full flex flex-col gap-5 justify-center">
           <p className="text-2xl font-medium text-center">My Work</p>
-          <div
-            className="bg-[#0e0e0e] mb-11 rounded-xl py-4 px-4 self-center gap-5 grid items-center justify-center content-center
+          <div layout layoutRoot
+            className="bg-[#0e0e0e] mb-11 rounded-xl py-4 px-4 self-center gap-5 grid items-start justify-center content-center
             w-[80%] grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3"
           >
-            <WorkPost title="Music App UI" imgSrc="media/musicapp.png" />
-            <WorkPost title="Movies App UI" imgSrc="media/moviesapp.png" />
+            <WorkPost title="Music App UI" imgSrc="media/musicapp.jpg" />
+            <WorkPost title="Movies App UI" imgSrc="media/moviesapp.jpg" />
             <WorkPost
               title="Transactions App UI"
-              imgSrc="media/transactionsapp.png"
+              imgSrc="media/transactionsapp.jpg"
               figProtoLink="https://www.figma.com/proto/wFFf9ryuyQZEu4avqtvdtq/Transactions-App?type=design&node-id=1-2&t=pr9s3mEdEwfOD7zR-1&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A2&mode=design"
             />
-            <WorkPost title="Articles App UI" imgSrc="media/articlesapp.png" />
+            <WorkPost title="Articles App UI" imgSrc="media/articlesapp.jpg" />
             <WorkPost
               title="Food App UI"
-              imgSrc="media/foodapp.png"
+              imgSrc="media/foodapp.jpg"
               figProtoLink="https://www.figma.com/proto/hgwZEp08VhEZTWDQdwykFS/Food-App?type=design&node-id=439-238&t=vDz2DnS6iSYap5hX-1&scaling=scale-down&page-id=439%3A6&starting-point-node-id=439%3A238&mode=design"
             />
           </div>
           <p className="text-2xl font-medium text-center">Get in touch</p>
-          <form className="flex justify-center w-full">
+          <form
+            className="flex justify-center w-full"
+            ref={form}
+            onSubmit={sendEmail}
+          >
             <div
               className="bg-[#0e0e0e] mb-11 rounded-xl py-4 px-4 self-center gap-5 grid
               w-[80%] max-w-[600px] grid-cols-1"
             >
               <input
                 className="w-full bg-[#222222] rounded-md p-2"
+                placeholder="Your name"
+                type="text"
+                name="from_name"
+                required
+              />
+              <input
+                className="w-full bg-[#222222] rounded-md p-2"
                 placeholder="Your email"
+                type="email"
+                name="email_id"
+                required
               />
               <textarea
                 className="w-full h-52 bg-[#222222] rounded-md p-2"
                 placeholder="Your message"
+                name="message"
+                required
               />
               <div className="flex justify-end w-full">
                 <button className="w-[150px] h-fit py-2 bg-[#333333] rounded-lg">
@@ -86,6 +132,7 @@ export default function Home() {
                 imgSrc="media/social-media-icons/dribbble_48px.png"
                 socialLink="https://dribbble.com/ahmedawsi"
               />
+              
             </div>
           </div>
         </div>
